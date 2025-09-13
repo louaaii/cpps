b4d404d5d35964df1e5ac4ced8905385b37729a9
louai <116638068+louaaii@users.noreply.github.com>
2025-09-13 22:16:16 +0200

    Add files via upload

---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/13 18:37:50 by marvin            #+#    #+#             */
+/*   Updated: 2025/09/13 18:37:50 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+private:
+    int                 value;
+    static const int    fractionalBits = 8;
+public:
+    Fixed();
+    ~Fixed();
+    Fixed(const Fixed& other);
+    Fixed &operator=(const Fixed& other);
+    Fixed(const int i);
+    Fixed(const float f);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+    float toFloat(void) const;
+    int toInt(void) const;
+    Fixed &min(Fixed &a, Fixed &b);
+    Fixed &max(Fixed &a, Fixed &b);
+    const Fixed &min(const Fixed &a, const Fixed &b);
+    const Fixed &max(const Fixed &a, const Fixed &b);
+
+    Fixed operator*(const Fixed &other) const;
+
+
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+#endif
