63c6a8032fe277954de920b739d80a7201426987
Louai Mokhtari <lmokhtar@f6r5s12.clusters.42paris.fr>
2025-09-16 17:35:03 +0200

    c fini jcrois

--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmokhtar <lmokhtar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 18:08:37 by lmokhtar          #+#    #+#             */
-/*   Updated: 2025/09/15 19:07:02 by lmokhtar         ###   ########.fr       */
+/*   Updated: 2025/09/16 17:34:30 by lmokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,9 @@ int main( void ) {
     d.attack("Cyril");
     c.takeDamage(20);
     c.guardGate();
+    f.attack("Houssein");
+    e.takeDamage(30);
+    e.beRepaired(40);
     f.highFivesGuys();
     e.highFivesGuys();
     
